a6e4c9ca84b423ef4c784573e26bd65b6f8e814c
hdusel <hdusel@3e333e5b-e5df-32b7-fd75-3871454d7834>
2010-09-26 19:35:15 +0000

    added: First Unittest (CPPUNIT) for threads.

---
+++ trunk/stateval/test/module/testCommon.h
@@ -0,0 +1,14 @@
+#ifndef _testCommon_H_
+#define _testCommon_H_ "2B35D852-C7BF-42DE-B938-CBB68A9EC5A9"
+
+#include <sys/time.h>
+
+static time_t getCurrentTimeInMS()
+{
+    struct timeval tv;
+    if (gettimeofday(&tv, NULL) != 0)
+        return 0;
+    return (tv.tv_sec*1000) + (tv.tv_usec/1000);
+}
+
+#endif // _testCommon_H_
