1931190972f249606b1021cbe0d6cb9aa01b5b17
hdusel <hdusel@3e333e5b-e5df-32b7-fd75-3871454d7834>
2010-09-26 13:17:47 +0000

    added: Implementation from Thread.h to src/Thread.cpp

--- trunk/stateval/include/stateval/Thread.h
+++ trunk/stateval/include/stateval/Thread.h
@@ -1,9 +1,6 @@
 #ifndef THREAD_H
 #define THREAD_H
 
-/* STD */
-#include <cassert>
-
 /* PThreads */
 #include <pthread.h>
 
@@ -15,30 +12,11 @@ class Mutex
   pthread_mutex_t m_Mutex;
 
 public:
-  Mutex()
-      : m_Mutex()
-  {
-    int rc = ::pthread_mutex_init(&m_Mutex, NULL);
-    assert(0 == rc);
-  }
+  Mutex();
+  ~Mutex();
 
-  ~Mutex()
-  {
-    int rc = ::pthread_mutex_destroy(&m_Mutex);
-    assert(0 == rc);
-  }
-
-  void lock()
-  {
-    int rc = ::pthread_mutex_lock(&m_Mutex);
-    assert(0 == rc);
-  }
-
-  void unlock()
-  {
-    int rc = ::pthread_mutex_unlock(&m_Mutex);
-    assert(0 == rc);
-  }
+  void lock();
+  void unlock();
 }; // class Mutex
 
 class MutexGrabber
@@ -46,17 +24,8 @@ class MutexGrabber
   Mutex& m_Mutex;
 
 public:
-  MutexGrabber(Mutex& inMutex)
-      : m_Mutex(inMutex)
-  {
-    m_Mutex.lock();
-  }
-
-  ~MutexGrabber()
-  {
-    m_Mutex.unlock();
-  }
-
+  MutexGrabber(Mutex& inMutex);
+  ~MutexGrabber();
 }; // class MutexGrabber
 
 class Condition
@@ -64,116 +33,42 @@ class Condition
   pthread_cond_t m_Condition;
 
 public:
-  Condition()
-      : m_Condition()
-  {
-    int rc = ::pthread_cond_init(&m_Condition, NULL);
-    assert(0 == rc);
-  }
+   Condition();
+  ~Condition();
 
-  ~Condition()
-  {
-    int rc = ::pthread_cond_destroy(&m_Condition);
-    assert(0 == rc);
-  }
-
-  void signal()
-  {
-    int rc = ::pthread_cond_signal(&m_Condition);
-    assert(0 == rc);
-  }
-
-  void wait(Mutex& inMutex)
-  {
-    int rc = ::pthread_cond_wait(&m_Condition, &inMutex.m_Mutex);
-    assert(0 == rc);
-  }
-
+  void signal();
+  void wait(Mutex& inMutex);
 }; // class Condition
 
 class Thread
 {
   mutable Mutex m_AccessGuard;
-  pthread_t m_ThreadHd;
+  pthread_t     m_ThreadHd;
 
   enum EState {eStopped, eStarting, eRunning, eStopping};
   EState m_State;
 
 public:
-  Thread()
-      : m_AccessGuard()
-      , m_ThreadHd(0)
-      , m_State(eStopped)
-  {}
+  Thread();
 
-  virtual ~Thread() {}
+  virtual ~Thread(){}
 
-  void start()
-  {
-    m_AccessGuard.lock();
-    if (eStopped == m_State)
-    {
-      m_State = eStarting;
-      int rc = ::pthread_create(&m_ThreadHd, NULL, _run, this);
-      assert(0==rc);
-      if (rc != 0)
-      {
-        m_State = eStopped; // cancel the starting state on failure
-      }
-    }
-    m_AccessGuard.unlock();
-  }
-
-  void cancel()
-  {
-    m_AccessGuard.lock();
-    if (eRunning == m_State)
-    {
-      m_State = eStopping;
-      m_AccessGuard.unlock();
-
-      signal_cancel();
-      join();
-
-      m_AccessGuard.lock();
-
-      assert(eStopped == m_State);
-    }
-    m_AccessGuard.unlock();
-  }
-
-  void join()
-  {
-    pthread_join(m_ThreadHd, NULL);
-  }
+  void start();
+  void cancel();
+  void join();
 
   bool isRunning() const
   {
     MutexGrabber grab(m_AccessGuard);
-
     return (eRunning == m_State);
   }
-
 
 protected:
   virtual void run() = 0;
   virtual void signal_cancel() {}
 
 private:
-  static void* _run(void* inArg)
-  {
-    Thread* inst = reinterpret_cast<Thread*> (inArg);
-    inst->m_AccessGuard.lock();
-    inst->m_State = eRunning;
-    inst->m_AccessGuard.unlock();
-
-    inst->run();
-
-    inst->m_AccessGuard.lock();
-    inst->m_State = eStopped;
-    inst->m_AccessGuard.unlock();
-    return NULL;
-  }
+  static void* _run(void* inArg);
 }; // class Thread
 
 } // end namespace Threading
