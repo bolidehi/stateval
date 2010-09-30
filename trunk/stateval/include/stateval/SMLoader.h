3a2f852adb1d97a94e0f194639481730991938bb
andreas.volz@tux-style.com <andreas.volz@tux-style.com@3e333e5b-e5df-32b7-fd75-3871454d7834>
2010-09-30 20:00:05 +0000

    some format docs

--- trunk/stateval/include/stateval/SMLoader.h
+++ trunk/stateval/include/stateval/SMLoader.h
@@ -29,9 +29,48 @@ public:
   int findMapingEvent (const std::string &event);
   
 private:
+  /*!
+   * Format description:
+   *
+   * file = states
+   * format = event1
+   *          event2
+   *          ...
+   *
+   */
   void fromEventsStream (std::ifstream &in_stream);
+
+  /*!
+   * Format description:
+   *
+   * file = states
+   * format = view:view_name:view_type:view_reference
+   *          map:from_event->to_event
+   *          map:from_event2->to_event2
+   *          ...
+   *
+   * the last parameter depends on view_type
+   */
   void fromViewsStream (std::ifstream &in_stream, const std::string &smDir);
+
+  /*!
+   * Format description:
+   *
+   * file = states
+   * format = state_name:state_type:parent_name[:history|view]
+   *
+   * the last parameter depends on state_type
+   */
   void fromStatesStream (std::ifstream &in_stream);
+
+  /*!
+   * Format description:
+   *
+   * file = transitions
+   * format = from_state_name:[event:]->to_state_name
+   *
+   * if no event is given than it's a default transition
+   */
   void fromTransitionsStream (std::ifstream &in_stream);
   
   std::vector <State*> mStateList;
