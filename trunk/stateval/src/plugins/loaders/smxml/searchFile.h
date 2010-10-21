61fb07967a067a726dc3f73d36e8c6b8ff684a7e
andreas.volz@tux-style.com <andreas.volz@tux-style.com@3e333e5b-e5df-32b7-fd75-3871454d7834>
2010-10-21 21:50:30 +0000

    two missing files

---
+++ trunk/stateval/src/plugins/loaders/smxml/searchFile.h
@@ -0,0 +1,15 @@
+#ifndef SEARCHFILE_H
+#define SEARCHFILE_H
+
+#include <string>
+#include <list>
+
+// public search functions
+
+/// ".so" is added....
+const std::string searchPluginFile (const std::string &type, const std::string &name);
+
+// private
+const std::string searchFile (std::list <std::string> &nameList);
+
+#endif // SEARCHFILE_H
